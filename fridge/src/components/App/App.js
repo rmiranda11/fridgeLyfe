53527cf00e0fb3a0446954bfb2232c5fe88756f6
rmiranda11 <r_miranda_11@yahoo.com>
2020-06-24 15:47:23 -0500

    StartUp

---
+++ fridge/src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {useState} from "react"
+
+function App(){
+
+    return(
+        <div>
+          <h1>Hello World</h1>
+            
+        </div>
+    )
+
+}
+
+export default App 
